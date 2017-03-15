ea36ef34bfa57064ea35954f65f8b17b3801f927
Barry hebbron <b.d.hebbron@outlook.com>
2017-03-15 16:02:20 +0000

    included cookies

---
+++ Forms5/Script/simpleCookies.js
@@ -0,0 +1,61 @@
+﻿function createCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+function deleteCookie(cname) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    document.cookie = cname + "=" + "" + ";" + expires + ";path=/";
+}
+
+function checkCookie(name) {
+    var cookie = readCookie(name);
+    if (cookie != "") {
+        return true;
+    } else {
+        return false;
+        }
+}
+
+
+function readCookie(cname) { //Take the cookiename as parameter (cname).
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie); //Decode the cookie string, to handle cookies with special characters, e.g. '$'
+        var ca = decodedCookie.split(';'); //Split document.cookie on semicolons into an array called ca (ca = decodedCookie.split(';')).
+        //Loop through the ca array (i = 0; i < ca.length; i++), and read out each value c = ca[i]).
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) { //If the cookie is found (c.indexOf(name) == 0), return the value of the cookie (c.substring(name.length, c.length).
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+
+function getAllCookies() {
+
+    var cookies = {};
+
+    if (document.cookie && document.cookie != '') {
+        var split = document.cookie.split(';');
+        for (var i = 0; i < split.length; i++) {
+            var name_value = split[i].split("=");
+            //  line removes leading space from the cookie name which will be present for the second and subsequent cookies
+            name_value[0] = name_value[0].replace(/^ /, '');
+            cookies[decodeURIComponent(name_value[0])] = decodeURIComponent(name_value[1]);
+        }
+    }
+
+    return cookies;
+
+}
+
--- Forms5/Script/simpleStorage.js
+++ Forms5/Script/simpleStorage.js
@@ -1,1 +1,53 @@
-﻿
+﻿function createItem(key, value) {
+    if (window.localStorage && !localStorage.getItem(key)) {
+        localStorage.setItem(key, JSON.stringify(value));
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function readItem(key) {
+    if (window.localStorage && localStorage.getItem(key)) {
+        return JSON.parse(localStorage.getItem(key));
+    } else {
+        return null;
+    }
+}
+
+function updateItem(key, value) {
+    if (window.localStorage && localStorage.getItem(key)) {
+        localStorage.removeItem(key);
+        localStorage.setItem(key, JSON.stringify(value));
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function deleteItem(key) {
+    if (window.localStorage && localStorage.getItem(key)) {
+        localStorage.removeItem(key);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getAllKeys() {
+    var keys = [];
+    if (window.localStorage.length > 0) {
+
+        for (var i = 0; i < localStorage.length; i++) {
+            keys[i] = localStorage.key(i);
+        }
+    }
+    return keys;
+}
+
+function clearAll() {
+    if (window.localStorage) {
+        localStorage.clear();
+        return true;
+    } else { return false }
+}
